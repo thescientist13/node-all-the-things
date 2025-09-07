0bc607d19bf6b6961a7942a59b9dfbfea9b3a845
Owen Buckley <owenbuckley13@gmail.com>
2025-09-07 13:32:46 -0400

    fix types

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,20 @@
-import Fastify from "fastify"
+import Fastify from "fastify";
 import { getGreeting } from "#lib/greeting.ts";
 
 const app = Fastify({
-  logger: true
+  logger: true,
 });
 
-app.get("/", async function handler (request) {
+app.get("/", async function handler(request) {
   // @ts-expect-error - TODO make this typesafe
   const name = request?.query?.name;
 
   return { message: getGreeting(name) };
 });
 
+app.get("/images", async function handler(request) {
+  // const response = await fetch('')
+});
+
 export { app };
+
